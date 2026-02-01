87a2df5fd30976a825a5a7be05835343cdf6b8e2
SSK233 <2370349890@qq.com>
2026-02-01 15:21:28 +0800

    删除部分注释

--- mainwindow.h
+++ mainwindow.h
@@ -44,12 +44,12 @@ public:
                    QPushButton *btn0_5, QPushButton *btn1, QPushButton *btn2,
                    QPushButton *btn2_2, QPushButton *btn5, QLineEdit *lineEdit, MainWindow *mainWindow, int rowIndex, int address);
 public:
-    QVector<bool> states;  // 改为public，方便外部访问
-    QLineEdit *lineEdit;   // 改为public，方便外部访问
+    QVector<bool> states;  
+    QLineEdit *lineEdit;   
     QSet<int> recentlyChangedRegisters;  // 跟踪最近修改的寄存器地址
     int registerAddress;    // 寄存器地址
     
-    void applyButtonStatesToUI();  // 改为public，方便外部访问
+    void applyButtonStatesToUI(); 
 
 private slots:
     void onButtonClicked();
